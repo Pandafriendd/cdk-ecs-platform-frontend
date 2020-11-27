d0882678812f7db2d85a1269a26cc9d2cfa665c1
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2020-11-27 18:40:39 +0000

    add a front end stack

--- bin/cdk-sg.js
+++ bin/cdk-sg.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const cdk = require('@aws-cdk/core');
 const { CdkSgStack } = require('../lib/cdk-sg-stack');
+const { CdkFrontendStack } = require('../lib/cdk-frontend-stack');
 
 const app = new cdk.App();
-new CdkSgStack(app, 'CdkSgStack');
+const envUSA = { account: '457175632986', region: 'us-east-1' };
+new CdkSgStack(app, 'CdkSgStack', { env: envUSA });
+new CdkFrontendStack(app, 'CdkFrontendStack', { env: envUSA });
